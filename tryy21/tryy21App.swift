e692d0d9f019383b75fd04c46021c0bb6ed74e9d
nooralotibi <nooreealotibi@gmail.com>
2022-10-27 11:32:11 +0300

    Initial Commit

---
+++ tryy21/tryy21App.swift
@@ -0,0 +1,17 @@
+//
+//  tryy21App.swift
+//  tryy21
+//
+//  Created by noor alotibi on 02/04/1444 AH.
+//
+
+import SwiftUI
+
+@main
+struct tryy21App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
